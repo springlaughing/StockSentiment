8aa29bb923669cb81e98b37aed7e6fcec30c97d5
Genya Gree <genya.gree@gmail.com>
2021-05-14 20:05:15 +0200

    Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='text_analyser',
+     version='0.0.1',
+     description='Analysing sentiment polarity of stock news headlines',
+     author='Eva Ivanova',
+     author_email='xxx@gmail.com',
+     license = 'MIT License: http://opensource.org/licenses/MIT',
+     packages=['sentiment_analysis'],
+     url = 'https://github.com/springlaughing/StockSentiment',
+     )
