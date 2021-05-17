c6c4a5265f6ea1800124f6d38bc40cc14d7e346d
Genya Gree <genya.gree@gmail.com>
2021-05-17 13:11:58 +0200

    Commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='StockSentiment',
      version='0.0.1',
      description='Analysing sentiment polarity of stock news headlines',
      author='springlaughing',
      license = 'MIT License: http://opensource.org/licenses/MIT',
-     packages=['StockSentiment'],
+     packages=find_packages(include=['StockSentiment.*']), zip_safe=False, 
      url = 'https://github.com/springlaughing/StockSentiment',
      )
